64a1cf97a64e88873ab9b7078f8d9b2467c6dc26
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-29 17:01:36 -0600

    fixing sintax errors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = !empty($_GET['url'] ? $_GET['url'] : 'home/home');
+$url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
 $arrUrl = explode("/" , $url);
 $controller = $arrUrl[0];
 $method = $arrUrl[0];
