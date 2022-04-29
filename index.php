b360333bce26d8effce60f3c4d59b196631d94b4
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-29 16:06:08 -0600

    creating variables

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<h1>Online Store</h1>
+<?php
+
+$url = !empty($_GET['url'] ? $_GET['url'] : 'home/home');
+$arrUrl = explode("/" , $url);
+$controller = $arrUrl[0];
+$method = $arrUrl[0];
+$params = "";
