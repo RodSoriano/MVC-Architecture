e6edce5cafeae68dc9bcde8fad9cfea9ae44dda4
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-02 12:06:55 -0600

    controller file updated

--- Libraries/Core/controllers.php
+++ Libraries/Core/controllers.php
@@ -4,11 +4,18 @@ class Controllers
     {
         public function __construct()
             {
-
+                $this->loadModel();
             }
 
         public function loadModel()
             {
-                
+                $model      = get_class($this) ."Model";
+                $routClass  = "Models/" . $model . ".php";
+
+                if(file_exists($routClass))
+                    {
+                        require_once($routClass);
+                        $this->model = new $model();
+                    }
             }
     }
