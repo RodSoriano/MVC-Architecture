15fda1fcc9b4a34867e5db4a3393a6a88035653d
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-05 14:15:57 -0600

    creating the first views

---
+++ Views/Errors/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Page not Found</title>
+
+</head>
+
+<body>
+    <h1>Page not Found</h1>
+</body>
+
+</html>
---
+++ Views/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Online Store</title>
+
+</head>
+
+<body>
+    <h1>Welcome to the site!</h1>
+</body>
+
+</html>
