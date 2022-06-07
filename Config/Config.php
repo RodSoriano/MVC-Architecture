aa7e1c146c824cbcfd4de57704586dbed662ab31
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-06 20:26:05 -0600

    db removed

--- Config/Config.php
+++ Config/Config.php
@@ -5,5 +5,5 @@ const BASE_URL      = "http://localhost/PHP-CRUD-Online-Store/";
 const DB_HOST       = "localhost";
 const DB_NAME       = "tienda";
 const DB_USER       = "cas";
-const DB_PASSWORD   = "Tango1023";
+const DB_PASSWORD   = "";
 const DB_CHARSET    = "charset=utf8";
