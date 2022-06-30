843b503366fb3a0de72feb9bb56a3883de1a2791
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-06-29 20:59:27 -0600

    new global variables

--- Config/Config.php
+++ Config/Config.php
@@ -2,8 +2,15 @@
 
 const BASE_URL      = "http://localhost/PHP-CRUD-Online-Store/";
 
+date_default_timezone_set('America/El_Salvador');
+
 const DB_HOST       = "localhost";
 const DB_NAME       = "tienda";
 const DB_USER       = "cas";
 const DB_PASSWORD   = "";
 const DB_CHARSET    = "charset=utf8";
+
+const SPD = ".";
+const SPM = ",";
+
+const SMONEY = "$";
